e35971be1fda1e9aa88bfc4fb4ff27116c1c58c9
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-04-24 16:22:37 -0600

    empezando a seguir el curso de php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nombre = "legna"
+
+?>
+
+<h1><?= $nombre . "  a " ?></h1>
+
+<style>
+    :root {
+        color-scheme: dark light;
+    }
+
+    html {
+        font-family: system-ui, sans-serif;
+    }
+</style>
